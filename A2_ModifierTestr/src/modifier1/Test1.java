765cb90aef2d2c1dd6e094908d0bb0e9d5ae4d82
krithikaece97 <101463114+krithikaece97@users.noreply.github.com>
2022-10-12 12:28:39 +0530

    Add files via upload

---
+++ A2_ModifierTestr/src/modifier1/Test1.java
@@ -0,0 +1,17 @@
+package modifier1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		//add();
+		System.out.println("main method");
+		add();
+
+	}
+	
+	public static void add()
+	{
+		System.out.println("add");
+	}
+
+}
---
+++ A2_ModifierTestr/src/modifier2/Test3.java
@@ -0,0 +1,13 @@
+package modifier2;
+
+import modifier1.Test1;
+
+public class Test3 extends Test1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add();
+
+	}
+
+}
